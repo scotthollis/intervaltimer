9a36f7541be8afca3ae14e0052db1baab6a4b6f3
Scott Hollis <scottdhollis@gmail.com>
2022-06-18 18:09:48 -0700

    progress blocks everyother interval and yellow coloring for break interval

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,9 @@
   import ProgressBlock from '../lib/ProgressBlock.svelte'
   let countDown = 30
   let sets = []
-  let index = 0
+  let index = 1
   let countDownColor = 'text-green-600'
+  let borderColor = 'border-slate-500'
   let buttonState = false
   function handleClick(){
     let tempCount = countDown
@@ -16,18 +17,25 @@
     if(countDown < 6){
       countDownColor = 'text-red-600'
     }
-    if(countDown === 0){
+    if((index % 2 == 0) && countDown === 0){
       countDown = 30
       countDownColor = 'text-green-600'
+      borderColor = 'border-slate-500'
       index++
+    }
+    else if(countDown === 0){
       sets = [...sets, index]
+      countDown = 30
+      countDownColor = 'text-yellow-600'
+      borderColor = 'border-yellow-500'
+      index++
     }
   }
 </script>
 
 <div class="text-center justify-center">
-  <h1 class="bg-green-100 p-4 border-b-2 border-slate-500">Interval Timer</h1>
-  <h2 class="m-10 text-9xl border-4 border-slate-500 rounded-full {countDownColor}">{countDown}</h2>
+  <h1 class="bg-green-100 p-4 border-b-2 border-slate-500 text-4xl">Interval Timer</h1>
+  <h2 class="m-10 text-9xl border-4 {borderColor} rounded-full {countDownColor}">{countDown}</h2>
 
   <button class="border rounded bg-blue-200 hover:bg-blue-300 p-2 disabled:bg-slate-100" disabled={buttonState} on:click={handleClick}>Start</button>
   <div id="sets" class="grid grid-cols-5 gap-4 m-4">
